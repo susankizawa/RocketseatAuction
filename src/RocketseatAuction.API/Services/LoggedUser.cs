1339e40d150629b746b8fd5ad2738bf709bee223
Susan <Susan@DESKTOP-EEL46Q3>
2024-02-08 17:58:00 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/Services/LoggedUser.cs
@@ -0,0 +1,38 @@
+﻿using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Services;
+
+public class LoggedUser : ILoggedUser
+{
+    private readonly IHttpContextAccessor _httpcontextAccessor;
+    private readonly IUserRepository _repository;
+
+    public LoggedUser(IHttpContextAccessor httpContext, IUserRepository repository)
+    {
+        _httpcontextAccessor = httpContext;
+        _repository = repository;
+    }
+
+    public User User()
+    {
+        var token = TokenOnRequest();
+        var email = FromBase64String(token);
+
+        return _repository.GetUserByEmail(email);
+    }
+
+    private string TokenOnRequest()
+    {
+        var authentication = _httpcontextAccessor.HttpContext!.Request.Headers.Authorization.ToString();
+
+        return authentication["Bearer ".Length..];
+    }
+
+    private string FromBase64String(string base64)
+    {
+        var data = Convert.FromBase64String(base64);
+
+        return System.Text.Encoding.UTF8.GetString(data);
+    }
+}
---
+++ tests/UseCases.Test/Auctions/GetCurrent/GetCurrentAuctionUseCaseTest.cs
@@ -0,0 +1,48 @@
+﻿using Bogus;
+using FluentAssertions;
+using Moq;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Enums;
+using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+using Xunit;
+
+namespace UseCases.Test.Auctions.GetCurrent;
+public class GetCurrentAuctionUseCaseTest
+{
+    [Fact]
+    public void Success()
+    {
+        //ARRANGE
+        var mock_auction = new Faker<Auction>()
+                            .RuleFor(auction => auction.Id, f => f.Random.Number(1, 100))
+                            .RuleFor(auction => auction.Name, f => f.Lorem.Word())
+                            .RuleFor(auction => auction.Starts, f => f.Date.Past())
+                            .RuleFor(auction => auction.Ends, f => f.Date.Past())
+                            .RuleFor(auction => auction.Items, (f, auction) => new List<Item>
+                            {
+                                new Item
+                                {
+                                    Id = f.Random.Number(1,100),
+                                    Name = f.Commerce.ProductName(),
+                                    Brand = f.Commerce.Department(),
+                                    BasePrice = f.Random.Decimal(50, 1000),
+                                    Condition = f.PickRandom<Condition>(),
+                                    AuctionId = auction.Id
+                                }
+                            }).Generate();
+
+        var auctionRepository = new Mock<IAuctionRepository>();
+        auctionRepository.Setup(i => i.GetCurrent()).Returns(mock_auction);
+
+        var useCase = new GetCurrentAuctionUseCase(auctionRepository.Object);
+
+        //ACT
+        var auction = useCase.Execute();
+
+        //ASSERT
+        auction.Should().NotBeNull();
+        auction.Id.Should().Be(mock_auction.Id);
+        auction.Name.Should().Be(mock_auction.Name);
+    }
+}
